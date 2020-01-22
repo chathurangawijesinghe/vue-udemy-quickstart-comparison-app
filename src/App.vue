ffc9eff360d3a072e6fbf33a7956145f169e6cb7
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-01-22 15:02:14 +0800

    router links

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
     <!-- <app-user></app-user> -->
+    <div>
+      <router-link to="/">Home</router-link>
+      <router-link to="/blog">Blog</router-link>
+      <router-link to="/account">Account</router-link>
+    </div>
     <router-view></router-view>
     <p>This is a test!</p>
   </div>
