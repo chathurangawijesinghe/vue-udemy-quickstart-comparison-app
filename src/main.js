d95684f7ad1a3d44877d75438a25772627160ba2
Chathuranga Wijesinghe <crwijesinghe@gmail.com>
2020-01-22 12:46:34 +0800

    components

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import App from './App.vue'
+import User from './components/User.vue'
+
+Vue.config.productionTip = false
+
+Vue.component('app-user', User);
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
